d1bc28b929c1ec07d8d4f5e5e878dd333489c028
Ismael Medeiros <140083162@aluno.unb.br>
2016-05-26 18:06:02 -0300

    Revert "#6 Adicionei parte do formulário do administrador"
    This reverts commit 178ba44b7b3bb951de8db7d4bb8d8d6f60bb0b7e.

--- ocorrencia/admin.py
+++ ocorrencia/admin.py
@@ -1,15 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-
-from .models import Categoria, Ocorrencia, Localizacao
-    
-
-class OcorrenciaAdmin(admin.ModelAdmin):
-    fields = ['data','hora','validade','atendida','emergencia','vitimado','descricao','resposta','usuario_id','vigilante_id','tb_categoria_id','tb_localizacao_id']
-    list_filter = ['data','tb_categoria_id']
-    list_display = ('data','hora','usuario_id','tb_categoria_id')
-
-admin.site.register(Categoria,)
-admin.site.register(Ocorrencia,OcorrenciaAdmin)
-admin.site.register(Localizacao,)
