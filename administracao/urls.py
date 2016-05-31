623ce05c5d29c93522ac0f7c4f79f8b629f0fc4d
Ismael Medeiros <140083162@aluno.unb.br>
2016-05-30 21:38:44 -0300

    Revert "Merge pull request #16 from eduardocalil/15-app-admin"
    This reverts commit 39cadf9e953993d32d38088714bc60108cbdf7f6, reversing
    changes made to a39de559c9c84d0ef7e33741b193fb543b809fbb.

--- administracao/urls.py
+++
@@ -1,8 +0,0 @@
-from administracao import views
-from django.conf.urls import url
-from .views import AdminView
-
-urlpatterns = [
-    url(r'^admin/$',
-        AdminView.as_view(), name='admin_list_apps'),
-]
--- unb_alerta/settings.py
+++ unb_alerta/settings.py
@@ -35,7 +35,6 @@ ALLOWED_HOSTS = []
 ALERTA_APPS = (
     'base',
     'ocorrencia',
-    'administracao'
 )
 
 INSTALLED_APPS = (
