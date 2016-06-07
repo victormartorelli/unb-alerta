9dfece106d3e15efce44f92a746fc4ba3670d106
Ismael Medeiros <140083162@aluno.unb.br>
2016-06-07 19:33:39 -0300

    #55 Retirei os lógica em comentários e arrumei o código para prestar o envio do email, porém ainda é preciso ter autorização da google pra enviar o email

--- unb_alerta/settings.py
+++ unb_alerta/settings.py
@@ -31,7 +31,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'unbalerta@gmail.com'
 EMAIL_HOST_PASSWORD = 'unbalerta123'
-EMAIL__USE_TLS = True
+EMAIL_USE_TLS = True
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login/?next='
--- usuario/views.py
+++ usuario/views.py
@@ -1,11 +1,8 @@
-# import string
-# import smtplib
 from django.core.mail import send_mail
 from django.views.generic import FormView
 from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
-# from unb_alerta.settings import EMAIL_HOST, EMAIL_PORT, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
 
 from .forms import UsuarioForm
 
@@ -32,18 +29,6 @@ class CriarUsuarioView(FormView):
         form = self.get_form(form_class)
 
         if form.is_valid():
-            # body = string.join((
-            #     "From: %s" % EMAIL_HOST_USER,
-            #     "To: %s" % [form.data.email],
-            #     "Subject: Confirmacao email",
-            #     "",
-            #     "Obrigado por se cadastrar no UnBAlerta."
-            # ), "\r\n")
-            # server = smtplib.SMTP(EMAIL_HOST + ':' + EMAIL_PORT)
-            # server.ehlo()
-            # server.starttls()
-            # server.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
-            # server.send(EMAIL_HOST, [form.data.email], body)
             send_mail(
                 'Confirmacao email',
                 'Obrigado por se cadastrar no UnBAlerta.',
