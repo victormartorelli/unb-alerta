c76947c086e34ac63f50683a51ab4900071f6bae
Eduardo Calil <dudusaid@gmail.com>
2016-06-07 18:01:05 -0300

    Melhora a validacao de ocorrencia e começa a tela de descricao

--- ocorrencia/urls.py
+++ ocorrencia/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-from django.views.generic.base import TemplateView
 
-from .views import (CriarOcorrenciaView, ListaOcorrenciasView,
+from .views import (CriarOcorrenciaView, DescricaoOcorrenciaView,
+                    ListaOcorrenciasView,
                     ListaValidacaoView, ValidarOcorrenciaEditView)
 
 urlpatterns = [
@@ -12,9 +12,7 @@ urlpatterns = [
         CriarOcorrenciaView.as_view(), name='criar_ocorrencia'),
 
     url(r'^ocorrencias/(?P<pk>\d+)$',
-        TemplateView.as_view(
-            template_name='ocorrencias/descricao_ocorrencia.html'),
-        name='ver_ocorrencia'),
+        DescricaoOcorrenciaView.as_view(), name='ver_ocorrencia'),
 
     url(r'^ocorrencias/validar$',
         ListaValidacaoView.as_view(), name='lista_validacao'),
--- ocorrencia/views.py
+++ ocorrencia/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import FormView, ListView, UpdateView
+from django.views.generic import (FormView, ListView, DetailView,
+                                  UpdateView)
 from django.contrib import messages
 from django.contrib.auth.mixins import (LoginRequiredMixin,
                                         PermissionRequiredMixin)
@@ -17,13 +18,16 @@ from .forms import OcorrenciaForm, ValidarOcorrenciaEditForm
 class ListaOcorrenciasView(LoginRequiredMixin, ListView):
     template_name = "ocorrencias/todas_ocorrencias.html"
     model = Ocorrencia
-    queryset = Ocorrencia.objects.filter(validade=True,
-                                         atendida=True)
+    queryset = Ocorrencia.objects.filter(
+        validade=True,
+        atendida=True).order_by('-id')
     paginate_by = 10
 
     def get_context_data(self, **kwargs):
         context = super(ListaOcorrenciasView, self).get_context_data(**kwargs)
-        # import ipdb; ipdb.set_trace()
+
+        if not context.get('object_list'):
+            context['vazio'] = True
 
         paginator = context['paginator']
         page_obj = context['page_obj']
@@ -80,15 +84,18 @@ class CriarOcorrenciaView(LoginRequiredMixin, FormView):
                 {'form': form})
 
 
-class ListaValidacaoView(LoginRequiredMixin, ListView):
+class ListaValidacaoView(PermissionRequiredMixin, ListView):
     template_name = "ocorrencias/validacao_list.html"
     model = Ocorrencia
-    queryset = Ocorrencia.objects.filter(atendida=False)
+    queryset = Ocorrencia.objects.filter(atendida=False).order_by('-id')
     paginate_by = 10
+    permission_required = {'ocorrencia.change_ocorrencia'}
 
     def get_context_data(self, **kwargs):
         context = super(ListaValidacaoView, self).get_context_data(**kwargs)
-        # import ipdb; ipdb.set_trace()
+
+        if not context.get('object_list'):
+            context['vazio'] = True
 
         paginator = context['paginator']
         page_obj = context['page_obj']
@@ -98,11 +105,12 @@ class ListaValidacaoView(LoginRequiredMixin, ListView):
         return context
 
 
-class ValidarOcorrenciaEditView(UpdateView):
+class ValidarOcorrenciaEditView(PermissionRequiredMixin, UpdateView):
     template_name = "ocorrencias/validacao_ocorrencia.html"
     form_class = ValidarOcorrenciaEditForm
     model = Ocorrencia
     success_url = reverse_lazy('lista_ocorrencias')
+    permission_required = {'ocorrencia.change_ocorrencia'}
 
     def get_context_data(self, **kwargs):
 
@@ -113,7 +121,15 @@ class ValidarOcorrenciaEditView(UpdateView):
     def get_initial(self):
         if self.request.user.is_superuser is False:
             usuario = Usuario.objects.get(user_id=self.request.user.id)
-            return {'vigilante_ID': usuario.id}
+            o = Ocorrencia.objects.get(id=self.kwargs['pk'])
+            return {'vigilante_ID': usuario.id,
+                    'emergencia': o.emergencia,
+                    'atendida': o.atendida,
+                    'validade': o.atendida,
+                    'repetida': o.repetida,
+                    'vitimado': o.vitimado,
+                    'foto': o.foto,
+                    'descricao': o.descricao}
         else:
             return {'vigilante_ID': 1}
 
@@ -138,3 +154,13 @@ class ValidarOcorrenciaEditView(UpdateView):
             messages.add_message(request, messages.ERROR, mensagem)
             return self.render_to_response(
                 {'form': form})
+
+
+class DescricaoOcorrenciaView(DetailView):
+    template_name = "ocorrencias/descricao_ocorrencia.html"
+
+    def get_queryset(self):
+        pk = self.kwargs['pk']
+        ocorrencia = Ocorrencia.objects.filter(id=pk)
+
+        return ocorrencia
