a8e89bfdb8e7b362560a9bccbb56a37c2b5ac72f
Ismael Medeiros <140083162@aluno.unb.br>
2016-06-07 18:53:57 -0300

    #55 Mudanças do Victor e adicionei um outro jeito para mandar o email. OBS: Os dois jeitos estão dando erro

--- usuario/views.py
+++ usuario/views.py
@@ -1,7 +1,11 @@
+# import string
+# import smtplib
+from django.core.mail import send_mail
 from django.views.generic import FormView
 from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
+# from unb_alerta.settings import EMAIL_HOST, EMAIL_PORT, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
 
 from .forms import UsuarioForm
 
@@ -28,6 +32,25 @@ class CriarUsuarioView(FormView):
         form = self.get_form(form_class)
 
         if form.is_valid():
+            # body = string.join((
+            #     "From: %s" % EMAIL_HOST_USER,
+            #     "To: %s" % [form.data.email],
+            #     "Subject: Confirmacao email",
+            #     "",
+            #     "Obrigado por se cadastrar no UnBAlerta."
+            # ), "\r\n")
+            # server = smtplib.SMTP(EMAIL_HOST + ':' + EMAIL_PORT)
+            # server.ehlo()
+            # server.starttls()
+            # server.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+            # server.send(EMAIL_HOST, [form.data.email], body)
+            send_mail(
+                'Confirmacao email',
+                'Obrigado por se cadastrar no UnBAlerta.',
+                'unbalerta@gmail.com',
+                [form.data.email],
+                fail_silently=False,
+            )
             return self.form_valid(form)
         else:
             mensagem = "Formulário Inválido"
