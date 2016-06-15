92e5195176e1682d7bfe979c7a8c5d29e94c75e6
victordantas <v.araujodantas@gmail.com>
2016-06-15 18:37:49 -0300

    recuperacao de senha feita, falta arrumar as telas

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import url
+from django.conf.urls import patterns, include, url
 from django.contrib.auth.views import login, logout
+from django.contrib.auth import views as auth_views
 
 from usuario.forms import LoginForm
 
@@ -17,5 +18,21 @@ urlpatterns = [
     url(r'^logout/$', logout, {'next_page': '/login'}, name='logout'),
 
     url(r'^perfil/$', PerfilView.as_view(), name='perfil'),
+
+    url(r'^reset/password_reset/$',
+        'django.contrib.auth.views.password_reset',
+        name='reset_password_reset1'),
+
+    url(r'^reset/password_reset/done/$',
+        'django.contrib.auth.views.password_reset_done',
+        name='password_reset_done'),
+
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$',
+        'django.contrib.auth.views.password_reset_confirm',
+        name='password_reset_confirm'),
+
+    url(r'^reset/done/$',
+        'django.contrib.auth.views.password_reset_complete',
+        name='password_reset_complete'),
 
 ]
