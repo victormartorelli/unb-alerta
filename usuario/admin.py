fcca2877c38af0268017c8f0aeda23176bdb3442
Kilmer Luiz <kilmerluizale@gmail.com>
2016-06-19 12:14:15 -0300

    Acrescentando filtros de grupo em usuario

--- usuario/admin.py
+++ usuario/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
 from .models import Usuario
 from django.contrib.auth.models import User
-from .filters import StatusFilter
+from .filters import StatusFilter, GroupFilter
 
+from django.forms import ModelForm, ValidationError, ChoiceField, RadioSelect
+from django import forms
+
+'''
+TODO 
+    Trocar seleção de sexo por select one
+'''
+class UsuarioAdminForm(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        fields = "__all__" 
+        sexo = ChoiceField(
+            label = 'Opcoes',
+            choices = (
+                (0, 'Don\'t change anything.'),
+                (1, 'Do some crazy stuff.'),
+            ),
+            initial = 0,
+            widget = RadioSelect,
+        )
 
 class UsuarioAdmin(admin.ModelAdmin):
+    form   = UsuarioAdminForm
     fields = ['login',
               'senha',
               'nome',
@@ -16,7 +37,7 @@ class UsuarioAdmin(admin.ModelAdmin):
               'data_nasc',
               'status',
               'grupo_usuario']
-    list_filter = ['sexo', 'data_nasc', 'grupo_usuario', StatusFilter]
+    list_filter = ['sexo', 'data_nasc', GroupFilter, StatusFilter]
     search_fields = ['nome', 'email']
     list_display = ['nome',
                     'email',
--- usuario/filters.py
+++ usuario/filters.py
@@ -2,9 +2,12 @@ from django.utils.translation import ugettext_lazy as _
 from django.db.models import Q
 from django.contrib import admin
 
+from django.contrib.admin import SimpleListFilter
+from django.contrib.auth.models import Group
+from django.utils.translation import ugettext as _
 
 class StatusFilter(admin.SimpleListFilter):
-    title = _('Status do usuário')
+    title = _('Status do Usuário')
     parameter_name = 'status'
 
     def lookups(self, request, model_admin):
@@ -18,3 +21,21 @@ class StatusFilter(admin.SimpleListFilter):
             return queryset.filter(status = 1)
         if self.value() == 'inativo':
             return queryset.filter(status = 0)
+
+
+class GroupFilter(SimpleListFilter):
+    title = _('Tipo de Usuário')
+    parameter_name = 'group'
+
+    def lookups(self, request, model_admin):
+        items = ()
+        for group in Group.objects.all():
+            items += ((str(group.id), str(group.name),),)
+        return items
+
+    def queryset(self, request, queryset):
+        group_id = request.GET.get(self.parameter_name, None)
+        if group_id:
+            return queryset.filter(groups=group_id)
+        return queryset
+
