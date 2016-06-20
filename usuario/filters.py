b33b259042eca1b3f7e511276e0cb4ceaa1157a3
Ismael Medeiros <140083162@aluno.unb.br>
2016-06-20 20:25:15 -0300

    #76 Finalizei filtro por idade

--- usuario/filters.py
+++ usuario/filters.py
@@ -2,11 +2,12 @@ import datetime
 
 from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
-from django.db.models import F
+from django.db.models import F, Q
 
 from django.contrib.admin import SimpleListFilter
 from django.contrib.auth.models import Group
 from django.utils.translation import ugettext as _
+
 
 class StatusFilter(admin.SimpleListFilter):
     title = _('Status do Usuário')
@@ -20,9 +21,9 @@ class StatusFilter(admin.SimpleListFilter):
 
     def queryset(self, request, queryset):
         if self.value() == 'ativo':
-            return queryset.filter(status = 1)
+            return queryset.filter(status=1)
         if self.value() == 'inativo':
-            return queryset.filter(status = 0)
+            return queryset.filter(status=0)
 
 
 class GroupFilter(SimpleListFilter):
@@ -41,6 +42,7 @@ class GroupFilter(SimpleListFilter):
             return queryset.filter(groups=group_id)
         return queryset
 
+
 class IdadeFilter(SimpleListFilter):
     title = _('Faixa Etária')
     parameter_name = 'data_nasc'
@@ -58,10 +60,58 @@ class IdadeFilter(SimpleListFilter):
         hoje = datetime.date.today()
 
         if self.value() == 'abaixo':
-            return queryset.filter((hoje.year - F('data_nasc_year') - ((hoje.month, hoje.day) < (F('data_nasc__month'), F('data_nasc__day')))) < 20)
+            return queryset.filter(
+                # Verifica se tem menos de 20 anos
+                Q(data_nasc__year__gt=(hoje.year - 20)) |
+                # Verifica se nasceu há 20 anos num mês depois do atual
+                Q(Q(data_nasc__year__exact=hoje.year - 20) & Q(data_nasc__month__gt=hoje.month)) |
+                # Verifica se nasceu há 20 anos, nesse mês e num dia depois do atual
+                Q(Q(data_nasc__year__exact=hoje.year - 20) & Q(data_nasc__month__exact=hoje.month) &
+                 Q(data_nasc__day__gt=hoje.day)))
         if self.value() == '2030anos':
-            return queryset.all()
+            return queryset.filter(
+                # Verifica se tem menos de 30 anos
+                Q(Q(data_nasc__year__gt=(hoje.year - 30)) |
+                # Verifica se nasceu há 30 anos num mês depois do atual
+                Q(Q(data_nasc__year__exact=hoje.year - 30) & Q(data_nasc__month__gt=hoje.month)) |
+                # Verifica se nasceu há 30 anos, nesse mês e num dia depois do atual
+                Q(Q(data_nasc__year__exact=hoje.year - 30) & Q(data_nasc__month__exact=hoje.month) &
+                  Q(data_nasc__day__gt=hoje.day))) &
+
+                # Verifica se tem 20 anos ou mais
+                Q(Q(data_nasc__year__lt=hoje.year - 20) |
+                # Verifica se nasceu há 20 anos num mês antes do atual
+                Q(Q(data_nasc__year__exact=hoje.year - 20) & Q(data_nasc__month__lt=hoje.month)) |
+                  # Verifica se nasceu há 20 anos, nesse mês e num dia antes ou igual ao atual
+                Q(Q(data_nasc__year__exact=hoje.year - 20) & Q(data_nasc__month__exact=hoje.month) &
+                  Q(data_nasc__day__lte=hoje.day)))
+            )
+
         if self.value() == '3040anos':
-            return queryset.all()
+            return queryset.filter(
+                # Verifica se tem menos de 40 anos
+                Q(Q(data_nasc__year__gt=(hoje.year - 40)) |
+                  # Verifica se nasceu há 40 anos num mês depois do atual
+                  Q(Q(data_nasc__year__exact=hoje.year - 40) & Q(data_nasc__month__gt=hoje.month)) |
+                  # Verifica se nasceu há 40 anos, nesse mês e num dia depois do atual
+                  Q(Q(data_nasc__year__exact=hoje.year - 40) & Q(data_nasc__month__exact=hoje.month) &
+                    Q(data_nasc__day__gt=hoje.day))) &
+
+                # Verifica se tem 30 anos ou mais
+                Q(Q(data_nasc__year__lt=hoje.year - 30) |
+                  # Verifica se nasceu há 30 anos num mês antes do atual
+                  Q(Q(data_nasc__year__exact=hoje.year - 30) & Q(data_nasc__month__lt=hoje.month)) |
+                  # Verifica se nasceu há 30 anos, nesse mês e num dia antes ou igual ao atual
+                  Q(Q(data_nasc__year__exact=hoje.year - 30) & Q(data_nasc__month__exact=hoje.month) &
+                    Q(data_nasc__day__lte=hoje.day)))
+            )
         if self.value() == 'acima':
-            return queryset.all()
+            return queryset.filter(
+                #Verifica se tem 40 anos ou mais
+                Q(data_nasc__year__lt=hoje.year - 40) |
+                # Verifica se nasceu há 40 anos num mês antes do atual
+                Q(Q(data_nasc__year__exact=hoje.year - 40) & Q(data_nasc__month__lt=hoje.month)) |
+                # Verifica se nasceu há 40 anos, nesse mês e num dia antes ou igual ao atual
+                Q(Q(data_nasc__year__exact=hoje.year - 40) & Q(data_nasc__month__exact=hoje.month) &
+                  Q(data_nasc__day__lte=hoje.day))
+            )
