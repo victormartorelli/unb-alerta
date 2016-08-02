4f4193aad065a48ba238e73fc4fd43740880b985
iurebrandao <iurebrandao@gmail.com>
2016-08-02 00:28:45 -0300

    Começo

--- static/js/mascaras.js
+++ static/js/mascaras.js
@@ -1,6 +1,13 @@
 function refreshMask () {
   $('.data').mask("00/00/0000", {placeholder: "__/__/____"});
-  $('.data').datepicker({dateFormat:'dd/mm/yy'});
+  $('.data').datepicker({
+    dateFormat:'dd/mm/yy',
+    monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+    monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
+    dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
+    dayNamesMin: ['D','S','T','Q','Q','S','S','D'],
+    dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
+  });
   $('.telefone').mask("00000000", {placeholder:"____-____"});
   $('.ddd').mask("00", {placeholder:"__"});
   $('.cpf').mask("000.000.000-00", {placeholder:"___.___.___-__"});
